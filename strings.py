775b464e8880387be835f33c6a8c8d6329c2e4ba
aditya-narayan-dubey <adityadubey306sir@gmail.com>
2025-07-11 18:00:29 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,18 @@
+course = "Python's learning program"
+print(course)
+name = 'hell "naah"'
+print(name)
+name2 = '''
+Hi Aditya,
+
+here is our first email
+
+thank you,
+The support team
+
+'''
+print(name2)
+course = 'python for beginners'
+another = course[:]
+print(another)
+
