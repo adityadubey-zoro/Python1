42d8644bbe9f75e10a3871a6e0bc400234763a75
aditya-narayan-dubey <adityadubey306sir@gmail.com>
2025-07-11 17:59:55 +0530

    Add files via upload

---
+++ string.methods.py
@@ -0,0 +1,9 @@
+course = 'Python for Beginners'
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.replace('Beginners', 'Intermediates'))
+print(course.count('Beginners'))
+print(course.find('Beginners'))
+print('python' in course)
+len()
